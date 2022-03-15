bd4f969f2824686db84a352d117d986e09a14f6e
Marcin Baran <baran.marcin1995@gmail.com>
2022-03-15 17:52:45 +0100

    add clock

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,25 @@
 let weatherForm = document.querySelector(".weatherForm")
-let apiURL = "https://api.weatherapi.com/v1/forecast.json?key=c5e4bb18b3544aff833124620221203&days=7&q="
+let apiURL = "http://api.weatherapi.com/v1/forecast.json?key=d0f25a3b42b9469cbc0181037221403&days=7&q="
 let apiDataContainer = document.querySelector(".apiData")
 let loader = document.querySelector(".loader")
+let secondsHands = document.querySelector(".handSeconds"),
+    minutesHands = document.querySelector(".handMinutes"),
+    hoursHands = document.querySelector(".handHours"),
+    now = new Date();
+let setupClock = function (){
+    let secs = now.getSeconds(),
+        mins = now.getMinutes() * 60,
+        hours = now.getHours() * 3600;
+
+    console.log(secs, mins, hours);
+
+    secondsHands.style.animationDelay = "-" + secs + "s";
+    minutesHands.style.animationDelay = "-" + mins + "s";
+    hoursHands.style.animationDelay = "-" + hours + "s";
+
+}
+
+setupClock();
 
 weatherForm.addEventListener("submit", (event) => {
     showLoader();
@@ -33,19 +51,19 @@ weatherForm.addEventListener("submit", (event) => {
             view += `<p>The amount of rainfall: ${dataFromAPI.current.precip_mm} mm</p>`
             view +=  `<p>Humidity: ${dataFromAPI.current.humidity} %</p>`
             view +=  `<p>Wind: ${dataFromAPI.current.wind_kph}km/h</p>`
-
-
             view += `</div>`
 
+            // Kończący div mainInfo
             view += `</div>`
 
             // DAYS
             view += `<div class="days">`
             dataFromAPI.forecast.forecastday.forEach((day) =>{
-                view += `<div class="days">`
-                view += `<div class="date">${day.date}</div>`
+                view += `<div class="daysInfo">`
+                view += `<div class="date"><p>Dnia</p>${day.date}</div>`
                 view += `<div class="icon"><img src="${day.day.condition.icon}" alt=""></div>`
                 view += `<div class="avgTemp">${day.day.avgtemp_c}<sup>o</sup>C</div>`
+                view += `</div>`
             })
             view += `</div>`
 
